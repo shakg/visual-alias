1900f907e30e7c3a23eb4d1d8c86cf755ffd91c6
İshak Gönül <ishak.gonul@titra.com.tr>
2022-08-24 11:54:59 +0300

    feat: :art: refactor off add alias

--- src/add-alias/add-alias.js
+++ src/add-alias/add-alias.js
@@ -1,8 +1,12 @@
-const aliasesFromLocalStorage = JSON.parse(localStorage.getItem("aliases"));
 
-if(!aliasesFromLocalStorage) throw Error;
-
+//cd /home/ishak/Desktop/mav-sdk/install/bin && ./mavsdk_server
 function addAlias(){
+    const aliasesFromLocalStorage = JSON.parse(localStorage.getItem("aliases"));
+
+    if(!aliasesFromLocalStorage){
+        localStorage.setItem("aliases", '[{}]')
+        aliasesFromLocalStorage = JSON.parse(localStorage.getItem("aliases"));
+    }
     const _name = document.getElementById("aliasName").value;
     const _self = document.getElementById("aliasSelf").value;
     const _aliasToAdd = {
